7df8a7a202b97cf76625b9de83fac806856ceead
Raine Virta <raine.virta@gmail.com>
2018-06-13 00:45:46 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# canyon-factory-outlet
+
+Scrape canyon factory outlet page to JSON format
+
+## usage
+
+```sh
+npm install
+./get-html | ./parse
+./get-table
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const cheerio = require('cheerio')
+const getStdin = require('get-stdin')
+
+const parseRawHtml = (rawHtml) => {
+  const $ = cheerio.load(rawHtml)
+  return $('article')
+    .map((i, e) => $(e).data())
+    .get()
+}
+
+const getDiscount = (article) =>
+  '-' +
+  (article.product.coupon ||
+    parseInt(article.product.metric1) -
+      parseInt(article.product.price) +
+      '.00')
+
+const processArticleDate = (article) => ({
+  name: article.product.name,
+  date: article.date,
+  price: article.product.price,
+  discount: getDiscount(article),
+  year: article.year,
+  size: article.size.replace(/\|/g,''),
+  wmn: !!article.wmn
+})
+
+getStdin()
+  .then(parseRawHtml)
+  .then(articles => articles.map(processArticleDate))
+  .then(bikes => console.log(JSON.stringify(bikes, null, 4)))
