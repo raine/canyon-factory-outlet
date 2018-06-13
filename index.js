b5b1e52f221f7fbc7e4e960a2ff4c12b6ec64658
Raine Virta <raine.virta@gmail.com>
2018-06-13 17:32:33 +0300

    add id to parsed output

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ const getDiscount = (article) =>
 const getWmn = (article) =>
   article.product.name.includes('WMN') || Boolean(article.wmn)
 
-const processArticleDate = (article) => ({
+const processArticle = (article) => ({
   name: article.product.name,
   date: article.date,
   price: article.product.price,
   discount: getDiscount(article),
   year: article.year,
   size: article.size.replace(/\|/g,''),
-  wmn: getWmn(article)
+  wmn: getWmn(article),
+  id: article.id
 })
 
 getStdin()
   .then(parseRawHtml)
-  .then(articles => articles.map(processArticleDate))
+  .then(articles => articles.map(processArticle))
   .then(bikes => console.log(JSON.stringify(bikes, null, 4)))
